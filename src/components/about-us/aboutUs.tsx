97914a1757b7f9d56290bd1f1e640a5c9ffff3ef
Luis David Lopez Zapata <luisdavidlz0202@gmail.com>
2023-04-23 00:41:45 -0500

    minor changes

--- src/components/about-us/aboutUs.tsx
+++ src/components/about-us/aboutUs.tsx
@@ -42,6 +42,11 @@ export default class AboutUs extends Component {
         title="Smart Classroom"
         description="SmartClassroom es una completa aplicación web que ofrece una amplia gama de funcionalidades para gestionar cursos y estudiantes de manera efectiva. A través de esta plataforma, es posible realizar el registro y la edición de cursos con detalles importantes como su nombre, descripción, fechas, profesor y otros atributos relevantes. Asimismo, se puede llevar a cabo el registro y la edición de estudiantes con datos básicos como su nombre, correo electrónico, teléfono y otros detalles de contacto."
       />
+      <blockquote>
+        <p dir="auto">Este proyecto se ha realizado con fines educativos, sin ánimo de lucro y con el objetivo meramente de aprender sobre las tecnologías con las que se ha implementado. Forma parte de la iniciativa impulsada por <a href="https://linktr.ee/trycatch.tv" rel="nofollow">TryCatch.TV</a> (Aprendizaje colaborativo) para realizar proyectos de forma colaborativa con varios desarrolladores que están apuntando a mejorar sus habilidades en ciertas áreas y para aquellos que están en búsqueda de sus primeros empleos como desarrolladores de software.</p>
+        <p dir="auto">Nosotros no somos responsables de cómo se use este proyecto ni de las consecuencias que puedan derivarse de su uso. Todos los participantes en este proyecto han contribuido de manera voluntaria y no se ha pagado a nadie por su colaboración. El código fuente de este proyecto se proporciona tal cual, sin garantías de ningún tipo.</p>
+        <p dir="auto">Si tienes alguna pregunta o comentario, no dudes en ponerte en contacto con nosotros.</p>
+      </blockquote>
         <Grid container spacing={2} justifyContent="center">
           {dataTeam.map((cardData: CardData, index: number) => (
             <Grid key={index} item xs={2.5}>
